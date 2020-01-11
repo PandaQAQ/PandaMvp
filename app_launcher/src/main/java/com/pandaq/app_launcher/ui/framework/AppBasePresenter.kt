5c3b148c998842ea1a24f99b9ed2d84c42fbb4fc
PandaQ <pand.h@foxmail.com>
2020-01-11 14:04:06 +0800

    [dev] 更新 BasePresenter V 的类型限制

--- app_launcher/src/main/java/com/pandaq/app_launcher/ui/framework/AppBasePresenter.kt
+++ app_launcher/src/main/java/com/pandaq/app_launcher/ui/framework/AppBasePresenter.kt
@@ -2,7 +2,9 @@ package com.pandaq.app_launcher.ui.framework
 
 import com.pandaq.app_launcher.net.ApiService
 import com.pandaq.appcore.framework.mvp.BasePresenter
+import com.pandaq.appcore.framework.mvp.IContract
 import com.pandaq.rxpanda.RxPanda
+import com.pandaq.rxpanda.exception.ApiException
 
 /**
  * Created by huxinyu on 2019/7/8.
@@ -10,10 +12,17 @@ import com.pandaq.rxpanda.RxPanda
  * <p>
  * Description :
  */
-open class AppBasePresenter<V>(view: V) : BasePresenter<V>(view) {
+open class AppBasePresenter<V:IContract.IMvpView>(view: V) : BasePresenter<V>(view) {
 
     val api: ApiService by lazy {
         return@lazy RxPanda.retrofit().create(ApiService::class.java)
+    }
+
+
+    protected fun handelError(e: ApiException?){
+        e?.let {
+            mView?.onError(e.code,e.message)
+        }
     }
 
 }
