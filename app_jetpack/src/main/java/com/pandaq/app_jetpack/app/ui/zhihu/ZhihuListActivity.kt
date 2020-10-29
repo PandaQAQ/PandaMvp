7394e16476d4035a9d0b56b473bb97a578eaf079
huxinyu <panda.h@foxmail.com>
2020-10-29 17:41:44 +0800

    [dev]demo 代码更新

--- app_jetpack/src/main/java/com/pandaq/app_jetpack/app/ui/zhihu/ZhihuListActivity.kt
+++ app_jetpack/src/main/java/com/pandaq/app_jetpack/app/ui/zhihu/ZhihuListActivity.kt
@@ -1,5 +1,6 @@
 package com.pandaq.app_jetpack.app.ui.zhihu
 
+import android.app.Activity
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
@@ -7,12 +8,14 @@ import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.alibaba.android.arouter.facade.annotation.Route
 import com.pandaq.app_jetpack.R
+import com.pandaq.app_jetpack.app.TestTask
 import com.pandaq.app_jetpack.app.ui.adapters.ZhihuNewsAdapter
 import com.pandaq.router.routers.RouterPath
+import com.pandaq.uires.msgwindow.Toaster
 import com.scwang.smartrefresh.layout.api.RefreshLayout
 import com.scwang.smartrefresh.layout.listener.OnRefreshLoadMoreListener
 import kotlinx.android.synthetic.main.a_activity_zhihu_list.*
-import okhttp3.OkHttpClient
+import java.util.concurrent.Executors
 
 /**
  * Created by huxinyu on 2020/3/30.
@@ -25,6 +28,17 @@ class ZhihuListActivity : AppCompatActivity() {
     private val viewModel by lazy {
         ViewModelProvider(this).get(ZhihuViewModel::class.java)
     }
+
+//    private val activityLauncher =
+//            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result: ActivityResult? ->
+//                if (result?.resultCode == Activity.RESULT_OK) {
+//                    Toaster.showSuccess(result.data?.getStringExtra("data"))
+//                }
+//            }
+//    private val requestPermission =
+//            registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) {
+//                Toaster.showSuccess("权限回调")
+//            }
 
     private var date: String = ""
     private var isRefresh = true
@@ -40,9 +54,11 @@ class ZhihuListActivity : AppCompatActivity() {
 
         viewModel.zhihuLiveData.observe(this, Observer {
             date = it.date.toString()
-            if (isRefresh){
+            if (isRefresh) {
+                rrv_data.finishRefresh(true)
                 adapter.setNewInstance(it.stories)
-            }else{
+            } else {
+                rrv_data.finishLoadMore(false)
                 adapter.addData(it.stories)
             }
         })
@@ -58,5 +74,19 @@ class ZhihuListActivity : AppCompatActivity() {
             }
 
         })
+
+        Executors.newFixedThreadPool(5)
+
+        adapter.setOnItemChildClickListener { _, _, _ ->
+//            val intent = Intent(this@ZhihuListActivity, MainActivity::class.java)
+//            activityLauncher.launch(intent)
+
+//            requestPermission.launch(arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE, android.Manifest.permission.WRITE_EXTERNAL_STORAGE))
+
+            val task  = TestTask()
+            task.execute("哈哈哈")
+        }
     }
+
+
 }
--- app_jetpack/src/main/java/com/pandaq/app_jetpack/app/ui/zhihu/ZhihuViewModel.kt
+++ app_jetpack/src/main/java/com/pandaq/app_jetpack/app/ui/zhihu/ZhihuViewModel.kt
@@ -6,6 +6,9 @@ import com.pandaq.app_jetpack.app.ui.base.BaseViewModel
 import com.pandaq.app_jetpack.app.net.AppCallBack
 import com.pandaq.rxpanda.transformer.RxScheduler
 import com.pandaq.uires.msgwindow.Toaster
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 /**
  * Created by huxinyu on 2020/3/30.
@@ -17,25 +20,36 @@ class ZhihuViewModel : BaseViewModel() {
     var zhihuLiveData: MutableLiveData<ZhihuData> = MutableLiveData()
 
     fun getDataList() {
-        api.test()
-                .doOnSubscribe {
-                    addDisposable(it)
-                }
-                .compose(RxScheduler.sync())
-                .subscribe(object : AppCallBack<ZhihuData>() {
-                    override fun success(data: ZhihuData) {
-                        zhihuLiveData.postValue(data)
-                    }
+        api.test().enqueue(object : Callback<ZhihuData> {
+            override fun onFailure(call: Call<ZhihuData>, t: Throwable) {
 
-                    override fun fail(code: Long?, msg: String?) {
-                        Toaster.showError(msg)
-                    }
+            }
 
-                    override fun finish(success: Boolean) {
+            override fun onResponse(call: Call<ZhihuData>, response: Response<ZhihuData>) {
+                println(Thread.currentThread().name)
+                zhihuLiveData.postValue(response.body())
+            }
 
-                    }
+        })
 
-                })
+//                .doOnSubscribe {
+//                    addDisposable(it)
+//                }
+//                .compose(RxScheduler.sync())
+//                .subscribe(object : AppCallBack<ZhihuData>() {
+//                    override fun success(data: ZhihuData) {
+//                        zhihuLiveData.postValue(data)
+//                    }
+//
+//                    override fun fail(code: Long?, msg: String?) {
+//                        Toaster.showError(msg)
+//                    }
+//
+//                    override fun finish(success: Boolean) {
+//
+//                    }
+//
+//                })
     }
 
     fun getHistory(date: String) {
