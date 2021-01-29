4802c65475fa59e9c878f1dc92736a0c0eccbeaa
huxinyu <panda.h@foxmail.com>
2021-01-29 11:34:33 +0800

    [dev]优化

--- uires/src/main/java/com/pandaq/uires/utils/Ext.kt
+++ uires/src/main/java/com/pandaq/uires/utils/Ext.kt
@@ -1,7 +1,9 @@
 package com.pandaq.uires.utils
 
+import android.view.View
 import com.afollestad.materialdialogs.MaterialDialog
 import com.pandaq.appcore.utils.system.DisplayUtils
+import com.pandaq.uires.R
 
 /**
  * Created by huxinyu on 2019/9/27.
@@ -13,4 +15,19 @@ fun MaterialDialog.compileSize(): MaterialDialog {
     params?.width = DisplayUtils.getScreenWidth() - DisplayUtils.dp2px(32f)
     this.window?.attributes = params
     return this
+}
+
+fun MaterialDialog.transWindow(): MaterialDialog {
+    this.window?.setBackgroundDrawableResource(R.color.btn_red_normal)
+    val parent = this.customView?.parent as View
+    parent.transAll()
+    return this
+}
+
+fun View.transAll() {
+    if (this.parent != null) {
+        val parent = this.parent as View
+        parent.setBackgroundResource(R.color.colorTrans)
+        parent.transAll()
+    }
 }
